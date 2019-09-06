c0653d09b5a435119f9244e4b4ab46734104a788
UnaLai <qqq323618@gmail.com>
2019-09-06 17:28:38 +0800

    [feat]:handle static file ex:css file

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const rootDir = path.dirname(process.mainModule.filename)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/',(req,res,next)=>{
+  res.sendFile(path.join(rootDir,'views','homepage.html'))
+})
+
+app.listen(3000)
